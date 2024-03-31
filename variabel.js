ef601c767ffd368cff46aef447bdde8a06ec67fc
essram <esarama22@gmail.com>
2024-03-31 23:26:19 +0700

    belajar jacasript

---
+++ variabel.js
@@ -0,0 +1,33 @@
+//variabel
+//var nama_variabel = nilaidata
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+
+// Jenis Tipe Data di Javascript
+// Tipe Data	Contoh
+// String	"Halo Dunia"
+// Boolean	true/false
+// Number	10, 9.15
+// Array	[1,2,3]
+// Object	{halo:"dunia"}
+
+//tipe data
+var nama = "Rama";
+var umur = 17;
+var tinggi = 179.9;
+console.log("Nama : " + nama);
+console.log("Umur : " + umur);
+console.log("Tinggi : " + tinggi);
+
+//cara cek tipe data
+typeof umur, tinggi, nama;
+console.log(typeof umur);
+console.log(typeof nama);
+console.log(typeof tinggi);
+
+//array merupakan salah satu jenis object
+var mobil = ["Porsche","Mercedes","BMW"];
+console.log(mobil);
+console.log(typeof mobil);
