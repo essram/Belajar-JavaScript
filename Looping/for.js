99393c573d956c9174d50457e73f17c41f0080e1
essram <esarama22@gmail.com>
2024-04-01 12:23:13 +0700

    Looping javaScript

---
+++ Looping/for.js
@@ -0,0 +1,13 @@
+//for loop
+// for(pernyataan1; pernyataan 2; pernyataan 3)
+// {
+//     //blog kode yang akan dijalankan
+// }
+// Penyataan 1 dijalankan (satu kali) sebelum eksekusi blok kode.
+// Pernyataan 2 mendefinisikan kondisi untuk mengeksekusi blok kode.
+// Pernyataan 3 dijalankan (setiap saat) setelah blok kode dijalankan.
+var teks = "";
+for (var i = 0; i < 5; i++) {
+  teks += "ini adalah nomor " + i + "<br>";
+}
+
---
+++ Looping/while.js
@@ -0,0 +1,12 @@
+//while loop (digunakan untuk pengulangan melalui blok kode selama kondisi terntenu benar)
+// while(kondisi)
+// {
+//     blok kode yang akan dijalankan
+// }
+teks = "";
+var i = 0;
+while(i<10)
+{
+    teks +="Ini adalah nomor " + i;
+    i++;
+}
